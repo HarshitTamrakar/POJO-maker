68c9fc49d651009d141e88677abcef13620f0d55
Harshit <tamrakarharshit@gmail.com>
2023-02-13 10:59:57 +0530

    initial commit

---
+++ src/main/java/com/harshit/Pojomaker/PojoMakerApplication.java
@@ -0,0 +1,13 @@
+package com.harshit.Pojomaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojoMakerApplication.class, args);
+	}
+
+}
