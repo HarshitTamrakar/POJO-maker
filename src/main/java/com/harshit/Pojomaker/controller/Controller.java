5e03114bb97d78d2ab8929fd8f098868d14ff7ee
Harshit <tamrakarharshit@gmail.com>
2023-02-20 09:09:03 +0530

    Updated controller

--- src/main/java/com/harshit/Pojomaker/controller/Controller.java
+++ src/main/java/com/harshit/Pojomaker/controller/Controller.java
@@ -1,5 +1,15 @@
 package com.harshit.Pojomaker.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.harshit.Pojomaker.exception.ParserException;
+import com.harshit.Pojomaker.model.Entity;
+import com.harshit.Pojomaker.service.Parser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +19,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/build")
 public class Controller {
 
+    private final Parser parserService;
+
+    @Autowired
+    public Controller(Parser parserService) {
+        this.parserService = parserService;
+    }
+
+
     @GetMapping
-    public void build() {
-        
+    public void getTemplate() {
+
     }
 
     @PostMapping
-    public void build2() {
-
+    public void parse(String jsonString) {
+        try (BufferedReader externalFileConfig = new BufferedReader(new InputStreamReader(
+            Objects.requireNonNull(Entity.class.getClassLoader()
+                .getResourceAsStream("demo.json"))))) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Entity entity = objectMapper.readValue(externalFileConfig, Entity.class);
+            parserService.pass(entity);
+        } catch (IOException | ParserException e) {
+            System.out.println(e);
+        }
     }
 
 }
